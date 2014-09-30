3a764dee20c0907a6f8c3d79d772784cf7c2f256
Kim <kim.walta@gatewaycdi.com>
2014-09-30 08:36:50 -0500

    adding style files from NPR UI project

---
+++ NPR2.0.8/Content/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('nprApp', []);
+
+
+app.controller('clientController', ['$scope', function ($scope) {
+  $scope.clients = [
+    { name: 'Google', ID: 10, contactName: 'Bryan', contactEmail: 'bryan@google.com', contactPhone: '555-555-5555', margin: '25%', acctManager: 'Jenna', mentor: 'Susan' },
+    { name: 'Cadillac', ID: 47 },
+    { name: 'Mercedes', ID: 3 }
+  ];
+}]);
+
+app.controller('campaignController', ['$scope', function ($scope) {
+  $scope.campaigns = [];
+}]);
+
+
+// Side nav expansion
+$(function () {
+  $("nav.main header a, nav.main a.open").click(function (e) {
+    e.preventDefault();
+    $("nav, main, footer").toggleClass("collapsed");
+
+    setTimeout(function () {
+      $(window).trigger("throttledresize", true);
+    }, 410);
+  });
+});
