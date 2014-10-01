a4c381b2a0dcb32313c08424f21f734e1d6b0495
Kim <kim.walta@gatewaycdi.com>
2014-10-01 15:46:39 -0500

    trying to get flex box to change on print

--- NPR2.0.8/App_Start/BundleConfig.cs
+++ NPR2.0.8/App_Start/BundleConfig.cs
@@ -30,7 +30,8 @@ namespace NPR2._0._8
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/style.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/style.css"));
 
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
