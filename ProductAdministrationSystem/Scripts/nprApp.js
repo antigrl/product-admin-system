c0f72cae69bc79f7fa470b62e38db1da455d50a0
Kim <kim.walta@gatewaycdi.com>
2015-04-24 11:33:04 -0500

    fixed to always show bottom save bar

--- ProductAdministrationSystem/Scripts/nprApp.js
+++ ProductAdministrationSystem/Scripts/nprApp.js
@@ -138,22 +138,3 @@ app.controller('AppCtrl', function($scope, $mdToast, $animate) {
     $mdToast.hide();
   };
 });
-
-$(function(){
-  //Keep track of last scroll
-  var lastScroll = 0;
-  $(window).scroll(function(event){
-      //Sets the current scroll position
-      var st = $(this).scrollTop();
-
-      //Determines up-or-down scrolling
-      if (st > lastScroll){
-        $(".bottom-save-bar").css("display",'inline')
-      }
-      if(st == 0){
-        $(".bottom-save-bar").css("display",'none')
-      }
-      //Updates scroll position
-      lastScroll = st;
-  });
-});
